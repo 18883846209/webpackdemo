9625a5b89c400b705d51a258e30199864a193893
zhangjie <1217336430@qq.com>
2017-12-15 16:39:59 +0800

    add

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -46,6 +46,15 @@ module.exports = {
 					name: utils.assetsPath('img/[name].[hash:7].[ext]')
 				}
 			},
+			// {
+			// 	test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+			// 	loader: 'url-loader',
+			// 	options: {
+			// 	limit: 10000,
+			// 	name: utils.assetsPath('media/[name].[hash:7].[ext]')
+			// 	//   useRelativePath: true
+			// 	}
+			// },
 			{
 				test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
 				loader: 'url-loader',
